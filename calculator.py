536be938d69ed1b22364702cda80faf050279450
Ihsan <ihsan_m.t@hotmail.com>
2026-02-20 22:26:35 +0100

    modifying divide

--- calculator.py
+++ calculator.py
@@ -9,3 +9,10 @@ def subs(x,y):
 def mult(x,y):
   '''multiplying two numbers'''
   return x * y
+
+def divide(x,y):
+  '''divide two numbers'''
+  if y == 0:
+    return "Error. Can't be zero"
+  else:
+    return x / y
